181fe8eb3324f1e7ce0a28f256caf2735cbe958f
Allure <1165275686@qq.com>
2024-06-28 23:25:14 +0800

    初步搭建项目框架

---
+++ springboot-init-master/src/main/java/com/lure/aiAnswer/service/PostService.java
@@ -0,0 +1,58 @@
+package com.lure.aiAnswer.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.lure.aiAnswer.model.vo.PostVO;
+import com.lure.aiAnswer.model.dto.post.PostQueryRequest;
+import com.lure.aiAnswer.model.entity.Post;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 帖子服务
+ *
+ */
+public interface PostService extends IService<Post> {
+
+    /**
+     * 校验
+     *
+     * @param post
+     * @param add
+     */
+    void validPost(Post post, boolean add);
+
+    /**
+     * 获取查询条件
+     *
+     * @param postQueryRequest
+     * @return
+     */
+    QueryWrapper<Post> getQueryWrapper(PostQueryRequest postQueryRequest);
+
+    /**
+     * 从 ES 查询
+     *
+     * @param postQueryRequest
+     * @return
+     */
+
+    /**
+     * 获取帖子封装
+     *
+     * @param post
+     * @param request
+     * @return
+     */
+    PostVO getPostVO(Post post, HttpServletRequest request);
+
+    /**
+     * 分页获取帖子封装
+     *
+     * @param postPage
+     * @param request
+     * @return
+     */
+    Page<PostVO> getPostVOPage(Page<Post> postPage, HttpServletRequest request);
+}
